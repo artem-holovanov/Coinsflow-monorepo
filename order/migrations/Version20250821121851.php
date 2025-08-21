14b5d5314755aa381cdec2c5895c2147f9e00c28
Artem Holovanov <artem.holovanov@stfalcon.com>
2025-08-21 16:20:21 +0300

    Generate migration

---
+++ order/migrations/Version20250821121851.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250821121851 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE "order_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE "order" (id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE product (id UUID NOT NULL, name VARCHAR(255) NOT NULL, price NUMERIC(7, 2) NOT NULL, quantity INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN product.id IS \'(DC2Type:uuid)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE "order_id_seq" CASCADE');
+        $this->addSql('DROP TABLE "order"');
+        $this->addSql('DROP TABLE product');
+    }
+}
