ce163e1c43f8e0038a6fbaf5f5fdc6568960a4fe
Artem Holovanov <artem.holovanov@stfalcon.com>
2025-08-21 16:24:47 +0300

    Regenerate migration

--- order/migrations/Version20250821132339.php
+++ order/migrations/Version20250821132339.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250821121851 extends AbstractMigration
+final class Version20250821132339 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,6 @@ final class Version20250821121851 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE "order_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE "order" (id INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE product (id UUID NOT NULL, name VARCHAR(255) NOT NULL, price NUMERIC(7, 2) NOT NULL, quantity INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN product.id IS \'(DC2Type:uuid)\'');
     }
@@ -30,8 +28,6 @@ final class Version20250821121851 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE "order_id_seq" CASCADE');
-        $this->addSql('DROP TABLE "order"');
         $this->addSql('DROP TABLE product');
     }
 }
