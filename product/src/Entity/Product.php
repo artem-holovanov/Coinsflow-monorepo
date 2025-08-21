bd9c182ce799249e8d6f7feb2db8fcd52fdea0d4
Artem Holovanov <artem.holovanov@stfalcon.com>
2025-08-21 11:25:26 +0300

    Extend Product from bundle

--- product/src/Entity/Product.php
+++ product/src/Entity/Product.php
@@ -2,80 +2,11 @@
 
 namespace App\Entity;
 
+use CommonBundle\Entity\Product as BaseProduct;
 use App\Repository\ProductRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
-use Symfony\Bridge\Doctrine\Types\UuidType;
-use Symfony\Component\Uid\Uuid;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
-class Product
+class Product extends BaseProduct
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
-    #[ORM\Column(type: UuidType::NAME, unique: true)]
-    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
-    private ?Uuid $id = null;
-
-    #[Assert\Type('string')]
-    #[Assert\Length(max: 255)]
-    #[Assert\NotBlank]
-    #[Assert\NotNull]
-    #[ORM\Column(type: Types::STRING, length: 255)]
-    private ?string $name = null;
-
-    #[Assert\NotBlank]
-    #[Assert\NotNull]
-    #[Assert\Regex(
-        pattern: '/^\d{1,5}(\.\d{1,2})?$/',
-        message: 'Price must be a number with up to 2 decimal places and max 7 digits total.'
-    )]
-    #[ORM\Column(type: Types::DECIMAL, precision: 7, scale: 2)]
-    private ?string $price = null;
-
-    #[Assert\Type('integer')]
-    #[Assert\Positive]
-    #[Assert\NotNull]
-    #[ORM\Column(type: Types::INTEGER)]
-    private ?int $quantity = null;
-
-    public function getId(): ?Uuid
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): Product
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function getPrice(): ?string
-    {
-        return $this->price;
-    }
-
-    public function setPrice(?string $price): Product
-    {
-        $this->price = $price;
-        return $this;
-    }
-
-    public function getQuantity(): ?int
-    {
-        return $this->quantity;
-    }
-
-    public function setQuantity(?int $quantity): Product
-    {
-        $this->quantity = $quantity;
-        return $this;
-    }
 }
